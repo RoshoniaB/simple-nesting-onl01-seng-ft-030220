80b4b3a2fc572eb3357fe2b67f130c0f478f55e2
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-03-14 20:29:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,7 +73,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    :yukihiro_matsumoto => {
+    :yukihiro_matsumoto => {}
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
     
