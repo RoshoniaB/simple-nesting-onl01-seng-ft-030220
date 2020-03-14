c6042f2e1adf04052cf973aae4308e30f33a982b
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-03-14 20:06:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,5 +1,5 @@
 
-def grace_hopper_known_for
+def hopper
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -15,13 +15,11 @@ def grace_hopper_known_for
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
 end
 
 def dennis_ritchies_language
